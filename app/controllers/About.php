9be7cff96fb02a54c218140773466a44f4f66e5b
ackermanjayjay <reza23198@gmail.com>
2020-09-09 20:46:01 +0700

    Perbaikan di Controller tidak muncul data saat menulis di url namun tidak error besok

---
+++ app/controllers/About.php
@@ -0,0 +1,12 @@
+<?php
+class About
+{
+  public function index($nama = 'Reza', $pekerjaan = 'Coolyeah')
+  {
+    echo "Aku ganteng $nama,$pekerjaan";
+  }
+  public function page()
+  {
+    echo 'About/page';
+  }
+}
